dc8e5a07e0c7cd7b6dc8d37f9cf92f72e07ab627
alexander-kurakin <kurakin.alexander.a@gmail.com>
2026-04-22 22:02:26 +1000

    TakeDamage added

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/TakeDamage/TakeDamageView.cs
@@ -0,0 +1,35 @@
+using System;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.TakeDamage
+{
+    public class TakeDamageView : EntityView
+    {
+        [SerializeField] private Transform _effectSpawnPoint;
+        [SerializeField] private ParticleSystem _takeDamageEffectPrefab;
+
+        private ReactiveEvent<float> _damageEvent;
+        private IDisposable _damageEventDisposable;
+        
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _damageEvent = entity.TakeDamageEvent;
+            _damageEventDisposable = _damageEvent.Subscribe(OnDamageTaken);
+        }
+
+        private void OnDamageTaken(float obj)
+        {
+            Instantiate(_takeDamageEffectPrefab, _effectSpawnPoint.position, Quaternion.identity);
+        }
+
+        public override void Cleanup(Entity entity)
+        {
+            base.Cleanup(entity);
+            
+            _damageEventDisposable.Dispose();
+        }
+    }
+}
