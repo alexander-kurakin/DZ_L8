8b04955930cc76c054f5fdcd0ea6441813049c6e
alexander-kurakin <kurakin.alexander.a@gmail.com>
2026-04-27 01:54:44 +1000

    Ranged enemy WIP

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/RangedEnemyConfig.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewRangedEnemyConfig", fileName = "RangedEnemyConfig")]
+    public class RangedEnemyConfig : EntityConfig
+    {
+        [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 9;
+        [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 900;
+        [field: SerializeField, Min(0)] public float MaxHealth { get; private set; } = 100;
+        [field: SerializeField, Min(0)] public float DeathProcessTime { get; private set; } = 1;
+        [field: SerializeField, Min(0)] public float AttackRange { get; private set; } = 15;
+        [field: SerializeField, Min(0)] public float AttackCooldown { get; private set; } = 2;
+        [field: SerializeField, Min(0)] public float InstantDamage { get; private set; } = 50;
+        [field: SerializeField, Min(0)] public float SpawnProcessTime { get; private set; } = 2;
+    }
+}
